8e3ad77adfa8a12021abacf9fe5e6954d1c883d6
Carlos David López Hinojosa <backupcarlos18@gmail.com>
2025-03-25 11:55:55 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Header from './components/Header'
+import Panel from './components/Panel'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Panel/>
+    </>
+  )
+}
+
+export default App
